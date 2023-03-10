0f2cee42a07e4c292ab95f661ac5293191152a5a
Vamsi Madhav H <vamsimadhav2000@gmail.com>
2023-03-10 09:30:48 +0530

    Added intial Repo

---
+++ Patterns/pattern2/pattern2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Printing a Square * Pattern" << endl;
+    cout << "Print no of rows and columns" << endl;
+    int row;
+    int column;
+    cin >> row;
+    cin >> column;
+
+    if (!(column == row)) {
+        cout << "Colum and row should be equal";
+        return 0;
+    }
+
+    for (int i=0; i<row; i++) {
+        for(int j=0; j<column; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+
+}
